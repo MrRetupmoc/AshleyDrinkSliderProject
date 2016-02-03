c62d03f37a6c5cba87ffe09fc92305b29e5442b1
MrRetupmoc's GitHub <MrRetupmoc42@gmail.com>
2016-02-02 21:55:55 -0500

    Update AshleyDrinkSlideDispensor.ino
    cleaned up logic and working on homing correctly now...

--- AshleyDrinkSlideDispensor.ino
+++ AshleyDrinkSlideDispensor.ino
@@ -1,3 +1,4 @@
+
 /*------------------------------------------------------------------------------------------------------
                               Author : Evan Richinson aka MrRetupmoc42
                
@@ -17,7 +18,7 @@ Febuary 2th 2016    : Updated / Merged Graphics into Controller, Updating more o
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_FT6206.h>
-#include <NewPing.h>
+//#include <NewPing.h>
 
 // For the Adafruit shield, these are the default.
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
@@ -32,7 +33,7 @@ Adafruit_FT6206 ctp = Adafruit_FT6206();
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 //Screen Data----------------------------------------------------------------------------------------- 
 
@@ -42,51 +43,53 @@ int Rotation = 1;
 
 //Movement Data-----------------------------------------------------------------------------------------
 
-//Stepper Pin Setup
+//Slide Stepper Pin Setup
 const int Slide_directionPin = 13;
 const int Slide_stepPin = 12;
 const int Slide_HomeLimitPin = 11;
 
+//Dispense Stepper Pin Setup
 const int Dispense_directionPin = 10;
 const int Dispense_stepPin = 9;
 const int Dispense_HomeLimitPin = 8;
 
-
-//Stepper Max Bounds Setup
+//Stepper Motors Max Bounds Setup
+const int Slide_stepdelay = 1;
 const int Slide_maxSteps = 500;
+
+const int Dispense_stepdelay = 1;
 const int Dispense_maxSteps = 20;
+
 int Slide_StepsCurrent = 0;
 
+//Slide Stepper Delay and Timings
+//int Slide_Move_Delay = 100;
+//int Dispense_Retract_Delay = 120;
+/*
 //Stepper Postiitonal Data
 const int Slide_Positions = 6;
 const int Slide_Positions_steps = Slide_maxSteps / Slide_Positions;
 
 //Dispense Timing
 int Dispense_mlPerSec = 120; // Untuned
-
-//Slide Stepper Delay and Timings
-const int Stepper_stepdelay = 1;
-int Slide_Move_Delay = 100;
-int Dispense_Retract_Delay = 120;
-
-//bool Dispense = false;
-
-int DistanceCm = 0;
-int DistanceCm_Offset = 6.6;
+*/
 
 
 //Array Data-----------------------------------------------------------------------------------------
 
-int DrinkChoice = 0;
 const int DrinkChoice_Max = 5;
+const int DrinkPositions_Max = 7;
+
+int DrinkChoice = 0;
+double DistanceCm = 0.0;
+double DistanceCm_Offset = 6.0;
 
 String DrinkName[DrinkChoice_Max] = {"  Tequila Sunrise", "     Screw Driver", "  Carribean Bliss", "    Whiskey Sour", "    Blissletoe"};
-//int DrinkDispenseTime[DrinkChoice_Max] = {3000 , 1500, 2000, 4123, 2392};
+int Recipy[DrinkChoice_Max][5] = { {2, 5, 5, 5, 0}, {1, 5, 5, 5, 0}, {1, 2, 5, 5, 5}, {3, 4, 4, 4, 0}, {1, 5, 6, 6, 6} };
 
-//                         6, 14, 22, 30, 38, 46, 
-int BottleLocations[6] = { 0, 8, 16, 24, 32, 40, 48 };
-int Recipy[Drink#,DrinkRecipie] = { { 2, 5, 5, 5 }, { 1, 5, 5, 5 }, { 1, 2, 5, 5, 5 } , {3, 4, 4, 4}, {1, 5, 6, 6, 6} };
-//int DrinkMix[DrinkChoice_Max, 6] = { {1,2,3,4,5,6}, {1,0,0,0,0,0}, };
+double BottleLocations[DrinkPositions_Max] = { 0.0, 8.0, 16.0, 24.0, 32.0, 40.0, 48.0 }; //6, 14, 22, 30, 38, 46, 
+
+//int DrinkDispenseTime[DrinkChoice_Max] = {3000 , 1500, 2000, 4123, 2392};
 
 //----------------------------------------------------------------------------------------
 
@@ -123,14 +126,13 @@ void setup() {
 
   SlideMovetoHome();
   tft.println("Drink Slide Homed...");
-  
 
+  
   if (Debug) Serial.println("Ready to Pour Drinks, Master");
   tft.println("Sucessfully Initicated...");
 
   delay(1000);
-  
-  DrawScreen(); 
+  DrawChoiceScreen(); 
 }
 
 
@@ -153,16 +155,13 @@ void loop(void) {
     DrawChoiceScreen();
   }
   else if (p.x > 120 && p.x < 180 && p.y > 100 && p.y < 230) { // Enter Area
-    //Dispense = true;
-    //DrawScreen();
     
     tft.println("     Dispensing Drink");
     
-    delay(DrinkDispenseTime[DrinkChoice]);
+    delay(1000);
 
     Dispense();
     
-    //Dispense = false;
     DrawChoiceScreen();
   }
   else if (p.x > 120 && p.x < 180 && p.y > 0 && p.y < 80) { // Forward Button
@@ -182,9 +181,18 @@ void loop(void) {
 }
 
 void DispenseMovetoHome() {
-  //when limit is hit set offset to ultrasonic value and real distance to 0
-  DistanceCm_offset = Distance;
-  DistanceCm = 0;
+  digitalWrite(Slide_stepPin, LOW);
+
+  //Move Slider to Home
+  do {
+    digitalWrite(Slide_directionPin, LOW);
+    delay(Dispense_stepdelay);
+    digitalWrite(Slide_directionPin, HIGH);
+    delay(Dispense_stepdelay);
+  }
+  while (digitalRead(Slide_HomeLimitPin)); //Do while not at Home
+
+  
 }
 
 //Moves Slider to Home
@@ -194,13 +202,19 @@ void SlideMovetoHome() {
   //Move Slider to Home
   do {
     digitalWrite(Slide_directionPin, LOW);
-    delay(Stepper_stepdelay);
+    delay(Slide_stepdelay);
     digitalWrite(Slide_directionPin, HIGH);
-    delay(Stepper_stepdelay);
+    delay(Slide_stepdelay);
   }
-  while (digitalRead(Slide_HomeLimitPin));
+  while (digitalRead(Slide_HomeLimitPin)); //Do while not at Home
 
-  int Slide_StepsCurrent = 0;
+  GetDistance();
+
+  //set offset to ultrasonic value and real distance to 0
+  DistanceCm_Offset = DistanceCm;
+  DistanceCm = 0;
+
+  Slide_StepsCurrent = 0;
 }
 
 //Moves Slider from Previous to Next Postition 
@@ -222,9 +236,9 @@ void SlideMovetoPosition(bool Direction, int MoveCount) {
     //Move Slider
     for(int i = 0; i < MoveCount; i++) {
       digitalWrite(Slide_directionPin, LOW);
-      delay(Stepper_stepdelay);
+      delay(Slide_stepdelay);
       digitalWrite(Slide_directionPin, HIGH);
-      delay(Stepper_stepdelay);
+      delay(Slide_stepdelay);
     }
     delay(Slide_Move_Delay);
     
@@ -241,23 +255,22 @@ void Dispense() {
   
   for(int i = 0; i < 50; i++) {
     digitalWrite(Dispense_directionPin, LOW);
-    delay(Stepper_stepdelay);
+    delay(Dispense_stepdelay);
     digitalWrite(Dispense_directionPin, HIGH);
-    delay(Stepper_stepdelay);
+    delay(Dispense_stepdelay);
   }
 
   //Dispense Proper Amount Drink Delay
-  delay(Dispense_Extend_Delay[(Slide_StepsCurrent / Slide_Positions_steps)]);
+  delay(1000);
 
   digitalWrite(Dispense_directionPin, HIGH);
   
   for(int i = 0; i < 50; i++) {
     digitalWrite(Dispense_directionPin, LOW);
-    delay(Stepper_stepdelay);
+    delay(Dispense_stepdelay);
     digitalWrite(Dispense_directionPin, HIGH);
-    delay(Stepper_stepdelay);
+    delay(Dispense_stepdelay);
   }
-  delay(Dispense_Retract_Delay);
 }
 
 void DrawChoiceScreen() {
@@ -285,8 +298,7 @@ void DrawChoiceScreen() {
     tft.setTextSize(2);
   //if(Dispense) tft.println("     Dispensing Drink");
 }
-
-
+/*
 void GetDistance() {
   unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
   DistanceCm = uS / US_ROUNDTRIP_CM; // Convert ping time to distance in cm and print result (0 = outside set distance range)
@@ -294,3 +306,4 @@ void GetDistance() {
   Serial.print(DistanceCm);
   Serial.println("cm");
 }
+*/
